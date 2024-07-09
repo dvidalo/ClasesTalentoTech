310a74eb679b17fc95a426e9aebedc8639a1f442
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-08 21:44:31 -0500

    se cargan as practicas de las variables y de la fabrica

---
+++ PracticaFabrica.py
@@ -0,0 +1,3 @@
+datoa=input("En una sola palabra define que sensacion da la cerveza: ")
+datob=input("En una Palabra define al Universo : ")
+print("posibles nombres \n\""+datoa+" "+datob+"\"\n\""+datob+" "+datoa+"\"")
---
+++ PracticaVariable.py
@@ -0,0 +1,7 @@
+nombre=input("Escribe tu nombre: ")
+apellido=input("Escribe tu Apellido: ")
+nombrecompleto=(nombre+" "+apellido)
+print("tu nombre completo es: "+nombrecompleto)
+print("\nEjercicio 2:")
+curso="python"
+print("Estas Tomando un Curso de "+curso)
