94bd4a5390fc55f018a11a2a7bd4e47db18b59ca
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-08 21:20:31 -0500

    cargue de todas las practicas del 08 de julio de 2024

---
+++ 03-input.py
@@ -0,0 +1,8 @@
+#recibir datos en un input
+input("digite su Primer nombre: ")
+
+#Pide e imprime
+print(input("Ingresa su Apellido: "))
+
+#imprimir en consola con texto
+print("tu segundo nombre es: "+ input("Digite su segundo Nombre: "))
---
+++ PracticaInput.py
@@ -0,0 +1,3 @@
+print("Tu estas Estudiando: "+input("\n¿Que estas estudiando?: "))
+print("Tu vives en: "+input("\nEn que Pais Vives: "))
+print("\nSu nombre completo es "+input("\nEscribe tu Nombre: ")+" "+input("\nEscribe tu apellido: "))
