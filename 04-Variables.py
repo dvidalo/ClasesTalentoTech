d1c7621ac9c4ccbfdd3d8de8c049cca3e5c29350
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-10 20:34:52 -0500

    se cargan los trabajos del dia 10-07-2024

---
+++ 04-Variables.py
@@ -0,0 +1,27 @@
+# nombre="Juan"
+# print(nombre)
+# nombre="Laura"
+# print(nombre)
+# edad = 30
+# print(edad)
+# edad1=35
+# edad2=50
+# print(edad1+edad2)
+# nombre =input("Digita tu nombre: ")
+# print("Tu nombre es: "+nombre)
+# nombre1="jhon"
+# nombre2="david"
+# nombres=nombre1+" "+nombre2
+# print(nombres)
+nombre ="Tony Soprano"
+edad = 51
+
+print("tu nombre es "+nombre+" y su edad es "+repr(edad))
+
+nombre= "julia"
+apellido="Roberths"
+nombrecompleto= nombre + " "+ apellido
+print("Su nombre es: " + nombrecompleto )
+
+curso="python"
+print("Estas Tomando un Curso de "+curso)
--- PracticaFabrica.py
+++ PracticaFabrica.py
@@ -1,3 +1,3 @@
 datoa=input("En una sola palabra define que sensacion da la cerveza: ")
-datob=input("En una Palabra define al Universo : ")
+datob=input("En una Palabra define al Universo debes mantener el genero de la respuesta anterior: ")
 print("posibles nombres \n\""+datoa+" "+datob+"\"\n\""+datob+" "+datoa+"\"")
