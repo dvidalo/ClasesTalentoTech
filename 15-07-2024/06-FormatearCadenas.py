4927ef41f70b15129179fd1e9a3dc720c03d33b4
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-15 18:38:26 -0500

    cargue clase 15-07-2024

---
+++ 15-07-2024/06-FormatearCadenas.py
@@ -0,0 +1,12 @@
+# se busca concatenar variables con string o cadenas
+# se da el valor a dos variables
+x = 10
+y = 5
+suma= x+y
+# se ingresan corchetes en las opciones donde quieres mostrar la variable 
+# y posterior se visualizen, se define con .format, que variables irian en cada corchete
+
+print("mis numeroes son {} y {}".format(x,y))
+print("mis numeroes son {} y {}".format(y,x))
+print("la suma de {} mas {} es igual a {}".format(x,y,suma))
+print("la suma de {} mas {} es igual a {}".format(x,y,x+y))
