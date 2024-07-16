edb91ccf420ea0b7023623fa0153183272a5dd0a
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-15 19:28:43 -0500

    practicas operadores matematicos

---
+++ 15-07-2024/PracticaOperadorMatematico.py
@@ -0,0 +1,29 @@
+
+import math
+
+# se utiliza la operacion division / para efectuar la operacion matematica
+x=874
+y=27
+print(x/y)
+
+# se utiliza el simbolo % para retomar el resto de una division
+x=456
+y=33
+print(x%y)
+
+# raiz cuadrada, se importa la libreria math
+x=783
+print(math.sqrt(x))
+
+# practica redondeo de cifras round(numero, decimales)
+
+x=10
+y=3
+d=x/y
+print(round(d,2))
+
+x=10.676767
+print(round(x))
+
+x=5
+print(round(math.sqrt(x),4))
