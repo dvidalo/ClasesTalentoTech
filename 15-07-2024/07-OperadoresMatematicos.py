26d97c88cc80fd6cdb720cccb0d30e37278b6799
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-15 20:18:49 -0500

    actualizacion Practicas Operadores y redondeo

---
+++ 15-07-2024/07-OperadoresMatematicos.py
@@ -0,0 +1,20 @@
+x=6
+y=2
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} dividido entre {y} es igual a {x/y}")
+
+# division al piso // da el resultado solo de la parte entera no redondea
+
+z=7
+
+print(f"{z} division al piso entre {y} es giual a {z//y}")
+
+# resto de una division %
+print(f"{z} modulo o resto de {y} es igual a {z%y}")
+
+# potenciar u nnumero **
+print(f"{x} elevado a {y} es igual a {x**y}")
+
+print(f"la raiz cuadrada de {x} es igual a {x**0.5}")
--- 15-07-2024/PracticaOperadorMatematico.py
+++ 15-07-2024/PracticaOperadorMatematico.py
@@ -2,28 +2,35 @@
 import math
 
 # se utiliza la operacion division / para efectuar la operacion matematica
+# se utiliza division al piso //
+# Practica Operadores Matematicos 1
 x=874
 y=27
-print(x/y)
+print(f"division al piso: {x//y}")
 
+# Practica Operadores Matematicos 2
 # se utiliza el simbolo % para retomar el resto de una division
 x=456
 y=33
-print(x%y)
+print(f"el resto es {x%y}")
 
-# raiz cuadrada, se importa la libreria math
+# Practica Operadores Matematicos 3
+# raiz cuadrada, se importa la libreria math o se eleva a la potencia 0.5
 x=783
-print(math.sqrt(x))
+
+print(f"potencia con math {math.sqrt(x)} o elevado a la potencia {x**0.5}")
 
 # practica redondeo de cifras round(numero, decimales)
-
+# Practica Redondeo 1
 x=10
 y=3
 d=x/y
-print(round(d,2))
+print(f"redondedo de la division a dos decimales  {round(d,2)}")
 
+# Practica Redondeo 2
 x=10.676767
-print(round(x))
+print(f"redondedo de al entero mas cercano {round(x)}")
 
+# PracticaRedondeo 3
 x=5
-print(round(math.sqrt(x),4))
+print(f"raiz cuadrada a rendondeo con 4 cifras {round(math.sqrt(x),4)}")
