460030288f933b78ca3c9a20849a4812c3523921
DIEGO FERNANDO VIDAL ORDONEZ <diego.fdo.vidal@gmail.com>
2024-07-15 21:20:29 -0500

    actualizacion clase 15-07-2024 y ejercicios

---
+++ 15-07-2024/08-Redondeo.py
@@ -0,0 +1,6 @@
+#para redondear utilizamos rouund(numero,decimales) 
+# se puede utilizar solo para obtener el entero round(numero)
+
+valor= 95.6666666666666
+print(round(valor))
+print(round(valor,2))
---
+++ 15-07-2024/09-Index.py
@@ -0,0 +1,5 @@
+x="marcela"
+print(x[3])
+print(x[1:6])
+print(x[2::2])
+print(x[::-1])
